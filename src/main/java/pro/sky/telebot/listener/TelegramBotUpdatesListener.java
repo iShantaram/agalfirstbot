e519d1868e4525b3c65501c54187a9bb725851ab
iShantaram <1agapovav987@gmail.com>
2024-02-19 22:04:58 +0300

    Бот обработал одно сообщение, добавил его в базу и перестал работать, стал выдавать ошибку при запуске.

--- src/main/java/pro/sky/telebot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telebot/listener/TelegramBotUpdatesListener.java
@@ -1,5 +1,9 @@
 package pro.sky.telebot.listener;
 
+import pro.sky.telebot.entity.NotificationTask;
+import pro.sky.telebot.repository.NotificationTaskRepository;
+
+
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
@@ -10,7 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -19,6 +27,12 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public TelegramBotUpdatesListener(NotificationTaskRepository notificationTaskRepository) {
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
 
     @PostConstruct
     public void init() {
@@ -32,10 +46,28 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             Long chatId = update.message().chat().id();
 
             if (update.message().text().equals("/start")) {
-                SendMessage message = new SendMessage(chatId, String.format("Привет, %s! \nВведи задачу в формате 01.01.2022" +
+                SendMessage message = new SendMessage(chatId, String.format("Привет, %s! \nВведи задачу в формате 01.02.2024" +
                                                        " 20:00 Оповещение!", update.message().from().firstName()));
                 telegramBot.execute(message);
             }
+
+            Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+            Matcher matcher = pattern.matcher(update.message().text());
+            String date = null;
+            String item = null;
+            if (matcher.matches()) {
+                date = matcher.group(1);
+                item = matcher.group(3);
+                logger.info("Date: {}, item: {}", date, item);
+            }
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+            if (date != null) {
+                LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+                notificationTaskRepository.save(new NotificationTask(chatId, item, dateTime));
+                SendMessage message = new SendMessage(chatId, "Событие сохранено!");
+                telegramBot.execute(message);
+            }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
