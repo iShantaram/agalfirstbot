48386f0f655d91f748e4f2e8fbb50b6e06aef847
iShantaram <1agapovav987@gmail.com>
2024-02-19 21:29:25 +0300

    Пошла обработка приветственного сообщения. Добавлены классы для задач и расписания

--- src/main/java/pro/sky/telebot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telebot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telebot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            Long chatId = update.message().chat().id();
+
+            if (update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(chatId, String.format("Привет, %s! \nВведи задачу в формате 01.01.2022" +
+                                                       " 20:00 Оповещение!", update.message().from().firstName()));
+                telegramBot.execute(message);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
